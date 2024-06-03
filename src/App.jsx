042fa9715e9c01c515033c635b4d27185a20df9d
tekla kvekvetsia <kvekveciatekla@gmail.com>
2024-06-03 13:55:26 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import NewProject from "./components/NewProject";
+import ProjectsSidebar from "./components/ProjectsSidebar";
+import NoProjectSelected from "./components/NoProjectSelected";
+import { useState } from "react";
+function App() {
+ const[projectsState,setProjectsState]= useState({
+   selectedProjectId:undefined,
+  projects:[]}
+ )
+ function handleStartAddProject(){
+  setProjectsState(prev=>{
+    return{
+      ...prev,
+      selectedProjectId:null,
+
+    }
+  })
+ }
+ let content;
+ if(projectsState.selectedProjectId===null){
+  content=<NewProject />
+ }else if(projectsState.selectedProjectId===undefined){
+  content=<NoProjectSelected onStartAddProject={handleStartAddProject}/>
+ }
+  return (
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
+    </main>
+  );
+}
+
+export default App;
